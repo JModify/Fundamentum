11007e7c492c36d603b60f5e0691aed6cc84b0ea
Modify <joshua.slaterr@gmail.com>
2021-10-26 13:46:40 +1000

    Test

---
+++ src/main/java/com/modify/fundamentum/exceptions/MySQLException.java
@@ -0,0 +1,13 @@
+package com.modify.fundamentum.exceptions;
+
+public class MySQLException extends Exception {
+
+    public MySQLException() {
+        super();
+    }
+
+    public MySQLException(String message) {
+        super(message);
+    }
+
+}
