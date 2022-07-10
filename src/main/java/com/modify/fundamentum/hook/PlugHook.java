68f72f7bad35117b127544f23e314069a8e6dbd9
Modify <joshua.slaterr@gmail.com>
2022-07-10 15:54:03 +1000

    = PlugHook updated to include dependent hooks.

--- src/main/java/com/modify/fundamentum/hook/PlugHook.java
+++ src/main/java/com/modify/fundamentum/hook/PlugHook.java
@@ -1,5 +1,6 @@
 package com.modify.fundamentum.hook;
 
+import com.modify.fundamentum.Fundamentum;
 import com.modify.fundamentum.text.PlugLogger;
 import lombok.Getter;
 import org.bukkit.Bukkit;
@@ -12,19 +13,28 @@ public abstract class PlugHook {
     /** Name of the plugin which is being hooked. */
     private String name;
 
+    /** Does this plugin depends on this hook to function properly */
+    private boolean depends;
+
     /**
      * Initializes a plugin hook.
      * @param name name of plugin to hook.
      */
-    public PlugHook(String name) {
+    public PlugHook(String name, boolean depends) {
         this.hooked = false;
         this.name = name;
+        this.depends = depends;
     }
 
     public void check() {
         if (Bukkit.getServer().getPluginManager().getPlugin(name) != null) {
             hooked = true;
             PlugLogger.logInfo(name + " detected. Plugin successfully hooked.");
+        } else {
+            if (depends) {
+                PlugLogger.logError("Failed to hook into dependency " + name + ". Plugin shutting down.");
+                Bukkit.getServer().getPluginManager().disablePlugin(Fundamentum.getPlugin());
+            }
         }
     }
 
